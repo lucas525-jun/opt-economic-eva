f412ac979717d33ed11e51f0d336a436755f92d4
GioZulber <giovannizulberti14@gmail.com>
2025-01-08 13:00:34 -0300

    Añadiendo ImpuestosController

--- src/main/java/mx/com/innovating/cloud/orchestrator/controller/ImpuestosController.java
+++ src/main/java/mx/com/innovating/cloud/orchestrator/controller/ImpuestosController.java
@@ -18,10 +18,12 @@ public class ImpuestosController {
     @Path("/createExcelImpuestos/{idOportunidad}/version/{version}")
     public Response createExcelImpuestos(@PathParam("idOportunidad") Integer idOportunidad, @PathParam("version") Integer version) {
         try {
+
             byte[] excel = impuestosServices.generateExcelImpuestos(idOportunidad, version);
             return Response.ok(excel)
                     .header("Content-Disposition", "attachment; filename=impuestos_"+idOportunidad+".xlsx")
                     .build();
+
         }catch (Exception e){
             e.printStackTrace();
             return Response.serverError().entity("Error al generar el archivo Excel").build();
