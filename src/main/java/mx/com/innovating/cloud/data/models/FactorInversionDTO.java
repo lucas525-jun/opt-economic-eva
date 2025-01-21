daf04930d013809cdadb42c17004fb69d5807ffe
PeterYang03110 <lightstar0831@gmail.com>
2025-01-21 23:25:30 +0200

    Add FactorInversion DTO.

---
+++ src/main/java/mx/com/innovating/cloud/data/models/FactorInversionDTO.java
@@ -0,0 +1,18 @@
+package mx.com.innovating.cloud.data.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FactorInversionDTO {
+    private Integer idhidrocarburo;
+    private Integer idoportunidadobjetivo;
+    private String hidrocarburo;
+    private Double fc_aceite;
+    private Double fc_gas;
+    private Double fc_condensado;
+    private Double mediapce;
+}
